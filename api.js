fbb387398b075c32ac98776ee520598f372b6e6a
Santz Development <snowdicsdorolax@gmail.com>
2026-03-12 12:28:39 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 async function consultaPlacasApp(placa){
 
     const res = await fetch("https://www.placas.app.br/",{
@@ -40,3 +42,52 @@ async function consultaPlacasApp(placa){
     };
 
 }
+
+const server = http.createServer(async (req,res)=>{
+
+    if(req.url.startsWith("/placa/")){
+
+        const placa = req.url.split("/")[2];
+
+        try{
+
+            const dados = await consultaPlacasApp(placa);
+
+            res.writeHead(200,{
+                "Content-Type":"application/json"
+            });
+
+            res.end(JSON.stringify({
+                placa:placa,
+                veiculo:dados
+            }));
+
+        }catch(e){
+
+            res.writeHead(500,{
+                "Content-Type":"application/json"
+            });
+
+            res.end(JSON.stringify({
+                erro:"não foi possível consultar a placa"
+            }));
+
+        }
+
+    }else{
+
+        res.writeHead(200,{
+            "Content-Type":"text/plain"
+        });
+
+        res.end("API consulta placa rodando");
+
+    }
+
+});
+
+server.listen(3000,"0.0.0.0",()=>{
+
+    console.log("API rodando na porta 3000");
+
+});
